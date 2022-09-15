aca4cde4c89872590f22d24ee51f36b12d92a535
kiranlandge <kiranlandge20@gmail.com>
2022-09-15 13:36:43 +0530

    i added new list

---
+++ kiran/src/com/exam/Demo.java
@@ -0,0 +1,19 @@
+package com.exam;
+
+import java.util.ArrayList;
+
+public class Demo {
+
+	public static void main(String[] args) {
+ArrayList<String>obj=new ArrayList<String>();
+obj.add("avi");
+obj.add("vishal");
+obj.add("shashi");
+obj.add("rituja");
+for(  String l: obj) {
+	System.out.println(l);
+	
+}
+	}
+
+}
